bed0262034ed3924bbe8dad8212b435b27ab1bff
Subhadip Maity <167627379+SontuCoder@users.noreply.github.com>
2026-07-03 18:01:46 +0530

    Refactor Navbar component with links and logo

--- src/components/sections/Nevbar/Navbar.tsx
+++ src/components/sections/Nevbar/Navbar.tsx
@@ -1,7 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+import { navigation } from "./Navbar_date";
+
+
+function Navbar_Links() {
+    return <>{
+        navigation.map((item) => (
+            <li key={item.href}>
+                <Link href={item.href}>{item.name}</Link>
+            </li>
+        ))
+    }
+    </>
+}
+
+function Navba_logo() {
+    return (
+        <Image
+            src="/vercel.svg"
+            alt="Subhadip Maity | AI Automation & Full Stack Developer"
+            width={40}
+            height={40}
+            className="border-2 border-white rounded-full bg-"
+            priority
+        />
+    )
+}
+
+
+
 export default function Navbar() {
     return (
         <header className="sticky top-0 z-50">
-            Navbar
+            <nav className="mx-auto flex h-16 items-center justify-between">
+                {/* Left */}
+                <div className="">
+                    {/* Logo */}
+                    <Navba_logo />
+
+                    <ul className="flex items-center gap-6 font-16">
+                        {/* Navigation Links */}
+                        <Navbar_Links />
+                    </ul>
+                </div>
+
+                {/* Right */}
+                <div>
+                    {/* Search */}
+                    {/* Theme Toggle */}
+                </div>
+            </nav>
         </header>
     );
 }
