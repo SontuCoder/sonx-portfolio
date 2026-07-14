ce26a4fdbaffa893a38efc7212114c4453057349
SontuCoder <subhadipmaity791@gmail.com>
2026-07-14 23:44:43 +0530

    Footer start

---
+++ src/components/sections/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/layout/Container";
+
+
+
+export default function Experience() {
+    return (
+        <footer className="">
+            <Container>
+                <></>
+            </Container>
+
+        </footer>
+    );
+}
